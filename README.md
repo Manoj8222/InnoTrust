3ad9a00f3c46be027303968eba792534d3c58451
Innovitegra Solutions <innovitegrasolutions@Innovitegras-MacBook-Air-2.local>
2025-02-09 10:08:59 +0530

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# react-native-cam-ocr-lib
+
+camera
+
+## Installation
+
+```sh
+npm install react-native-cam-ocr-lib
+```
+
+## Usage
+
+
+```js
+import { multiply } from 'react-native-cam-ocr-lib';
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'react-native-cam-ocr-lib' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+const CamOcrLib = NativeModules.CamOcrLib
+  ? NativeModules.CamOcrLib
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export function multiply(a: number, b: number): Promise<number> {
+  return CamOcrLib.multiply(a, b);
+}
